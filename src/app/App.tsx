82e1d479394e6954aacf875520c4f89e12c67930
Sergey Petrosyan <petrosyanserg33@gmail.com>
2025-08-17 13:54:48 +0400

    fix build

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,5 @@
-import Providers from "src/app/Providers.tsx";
 import AppRouter from "src/route/routes.tsx";
 
-const App = () => {
-  
-  return (
-    <Providers>
-      <AppRouter/>
-    </Providers>
-  )
-}
+const App = () => (<AppRouter/>)
 
 export default App
